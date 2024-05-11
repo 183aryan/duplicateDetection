5982ff1815b96e593d0c140651721020b3049ca1
183aryan <aryan183choudhary@gmail.com>
2024-05-11 23:10:11 +0530

    UI and error handling

--- streamlit-app/app.py
+++ streamlit-app/app.py
@@ -4,18 +4,28 @@ import pickle
 
 model = pickle.load(open('model.pkl','rb'))
 
-st.header('Duplicate Question Pairs')
+# to place image in center
+
+st.image('duplicatedetect.png', width=700)
+# st.image('duplicatedetect.png', width = 700)
+
+st.title('Duplicate Detection')
+# st.header('Duplicate Question Pairs')
 
 q1 = st.text_input('Enter question 1')
 q2 = st.text_input('Enter question 2')
 
 if st.button('Find'):
     query = helper.query_point_creator(q1,q2)
-    result = model.predict(query)[0]
-
-    if result:
-        st.header('Duplicate')
+    if(q1 == '' or q2 == ''):
+        st.warning('Please enter both questions')
+        # st.stop()
     else:
-        st.header('Not Duplicate')
+        result = model.predict(query)[0]
+        st.success('Prediction Successful')
+        if result:
+            st.markdown('#### Duplicate')
+        else:
+            st.markdown('#### Not Duplicate')
 
 
